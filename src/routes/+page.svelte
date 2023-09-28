b640bcf01b3de94995ee1ad030269524cbfb7280
chronitch <chronitch@outlook.com>
2023-09-28 22:19:05 +0200

    Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import App from '$lib/components/App.svelte'
-	import Countdown from '../lib/components/Countdown.svelte';
+	import Maintenence from '$lib/components/Maintenence.svelte';
 
   let maintanence = true;
 </script>
@@ -10,15 +10,7 @@
   <App />
 </div>
 {:else}
-<div class="flex flex-col">
-  <div class="flex flex-col text-center justify-center gap-y-10">
-    <h1 class="text-6xl capitalize font-bold">We are under <span class="text-accent">maintenance</span></h1>
-    <p>Estimeted time</p>  
-    <Countdown/>
-    <p>Our socials</p>
-  </div>
-  <a class="underline text-primary text-center mb-11" href="https://twitter.com/metawars_off"><i class="fi fi-brands-twitter"></i>Twitter</a>
-</div>
+  <Maintenence/>
 {/if}
 
 <style>
